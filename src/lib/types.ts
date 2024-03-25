c302676eaa6282d292fc190132e792a1a4c717a1
rdyselinck <rune.dyselinck@ugent.be>
2024-03-25 21:13:29 +0100

    Equal laps now have equal position

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,14 +1,15 @@
 export type Count = {
-  count: number,
-  team: {
-    id: number,
-    name: string,
-  }
+    count: number,
+    rank: number,
+    team: {
+        id: number,
+        name: string,
+    }
 };
 
 export type SocketMsg<T = unknown> = {
-  topic: string;
-  data: T
+    topic: string;
+    data: T
 }
 
 export type SocketHandler = (msg: SocketMsg<any>) => void;
